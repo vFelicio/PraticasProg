c10e481f48208d1c1c59a1f15da3d75dc1729376
vFelicio <61336711+vFelicio@users.noreply.github.com>
2020-03-17 15:39:57 -0300

    Versão 17-03

---
+++ InserirPais.sql
@@ -0,0 +1,16 @@
+use mydb;
+insert into pais (nome, populacao, area)
+values ('Brasil', 210147125, 8515727);
+insert into pais (nome, populacao, area)
+values ('Estados Unidos', 328700000, 9371174);
+insert into pais (nome, populacao, area)
+values ('Italia', 60390560, 301338);
+insert into pais (nome, populacao, area)
+values ('China', 1394550000, 9596961);
+insert into pais (nome, populacao, area)
+values ('Espanha', 46733038, 504782);
+insert into pais (nome, populacao, area)
+values ('Argentina', 44938712, 2780400);
+insert into pais (nome, populacao, area)
+values ('Coreia do Sul', 51811167, 99016);
+
